507197e9369e8e70a06a28bb98e230f8ca6964d7
NovaSaga <david.kezele@hotmail.com>
2024-08-04 21:53:54 +0200

    version 1

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,18 @@
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IdeaController;
 
 //feed
 
 //profile
 
-Route::get('/', [DashboardController::class, 'index']);
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
-Route::get('/profile', [ProfileController::class, 'index']);
+Route::post('/ideas', [IdeaController::class, 'store'])->name('ideas.store');
+
+Route::delete('/ideas/{id}', [IdeaController::class, 'destroy'])->name('ideas.destroy');
+
+Route::get('/terms', function(){
+    return view('terms');
+});
