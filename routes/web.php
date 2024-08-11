79a1b71a8fe295a81bf0dcf40e6f740352aa79e3
NovaSaga <david.kezele@hotmail.com>
2024-08-11 12:01:32 +0200

    like system

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IdeaController;
 use App\Http\Controllers\FollowerController;
 use App\Http\Controllers\TermsController;
+use App\Http\Controllers\IdeaLikeController;
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
@@ -24,6 +25,9 @@ Route::resource('users', UserController::class)->only(['show']);
 
 Route::post('users/{user}/follow', [FollowerController::class ,'follow'])->middleware('auth')->name('users.follow');
 Route::post('users/{user}/unfollow', [FollowerController::class ,'unfollow'])->middleware('auth')->name('users.unfollow');
+
+Route::post('ideas/{idea}/like', [IdeaLikeController::class ,'like'])->middleware('auth')->name('ideas.like');
+Route::post('ideas/{idea}/unlike', [IdeaLikeController::class ,'unlike'])->middleware('auth')->name('ideas.unlike');
 
 Route::get('terms', [TermsController::class, 'show'])->name('terms');
 
